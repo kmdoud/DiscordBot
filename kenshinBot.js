500fb3aed0d4f32a3a55dc70fa52dde701b44bab
Ken Doud <kenneth.doud@gmail.com>
2020-10-22 13:55:06 -0400

    added admin folder, simple command handler, edits and reactions, private message on command, and clicking reactions to assign roles

--- kenshinBot.js
+++ kenshinBot.js
@@ -1,12 +1,14 @@
 const cmd = '!';
 var discord = require('discord.js');
 var bot = new discord.Client();
-var token = require('./token.js')
-var package = require('./package.json');
+var token = require('./token.js');
+var welcome = require('./welcome.js');
 var tokenClass = require('./token.js');
 var helloClass = require('./hello.js');
-var welcome = require('./welcome.js');
+var package = require('./package.json');
 var json = require('./appsettings.json');
+const config = require('./Admin/config.json');
+const command = require('./Commands/command');
 var shadyPlebClass = require('./shadyPleb.js');
 var apexAssemblyClass = require('./apexAssembly.js');
 var chelAssemblyClass = require('./chelAssembly.js');
@@ -14,141 +16,171 @@ var raidAssemblyClass = require('./raidAssembly.js');
 var zoneAssemblyClass = require('./zoneAssembly.js');
 var checkPermissionClass = require('./checkPermission.js');
 var invalidPermissionClass = require('./invalidPermission');
-var playerInfoClass = require('./embeds/playerInfoEmbed.js');
 var cmdListEmbedClass = require('./embeds/commandListEmbed.js');
 var sirMasterProgrammerClass = require('./sirMasterProgrammer.js');
+var firstMessage = require('./Edits and Reactions/first-message.js');
+var privateMessage = require('./Private Messages/private-message.js');
+const roleClaim = require('./Roles on Reaction/role-claim.js');
 
  
 
 bot.on
 ('ready', () => 
 {
-    console.log(`ShadyBot version: ${package.version} is online!`);
+    console.log(`ShadyBot version: ${package.version} is online!`)
+    //sends message to user when added to server
     welcome(bot);
+    //response command
+    command(bot, 'ping', (message) => 
+    {
+        //let hasRole = checkPermissionClass.checkPermission(message)
+        //var invalidMessage = invalidPermissionClass.invalidPermission;
+        message.channel.send('pong')
+    })
+
+    //posts a message in a specified channel with reactions
+    firstMessage(bot, '768587987722174484', 'hello world!!!', ['👍', '🏢'])
+
+    //returns a message with the total number of members in a server
+    // command(bot, 'servers', (message) =>
+    // {
+    //     bot.guilds.cache.forEach((guild) => 
+    //     {
+    //         message.channel.send
+    //         (
+    //             `${guild.name} has a total of ${guild.memberCount} members`
+    //         )
+    //     })
+    // })
+
+    //clear all messages within a channel with admin permissions
+    command(bot, ['cc', 'clearchannel'], message =>
+    {
+        if(message.member.hasPermission(`ADMINISTRATOR`))
+        {
+            message.channel.messages.fetch().then(results => 
+                {
+                  message.channel.bulkDelete(results);      
+                })
+        }
+    })
+
+    //change the status of the bot
+    command(bot, 'status', (message) =>
+    {
+        const content = message.content.replace('!status', '')
+        bot.user.setPresence
+        ({
+            activity: 
+            {
+                name: content,
+                type: 0,
+            },
+        })
+    })
+
+    //send user private message response
+    privateMessage(bot, 'ping', 'pong')
+    //send user private message
+    // bot.users.fetch('507233660626403356').then((user) =>
+    // {
+    //     user.send('Hello World!')
+    // })
+
+    //create text channel under specific category
+    // command(bot, 'createtextchannel', (message) =>
+    // {
+    //     var content = message.content.replace('!createtextchannel ', '')
+    //     message.guild.channels.create(content, 
+    //         {
+    //             type: 'text'
+    //         }).then((channel) =>
+    //         {
+    //             console.log(channel)
+    //             var categoryId = '768504781051527178'
+    //             channel.setParent(categoryId)
+    //         })
+    // })
+
+    //create voice channel under specific category
+    command(bot, 'creatvoicechannel', (message) =>
+    {
+        var name = message.content.replace('!createvoicechannel', '')
+        message.guild.channels.create(name, 
+            {
+                type: 'voice'
+            }).then((channel) => 
+            {
+                var categoryId = '768504781051527178'
+                channel.setParent(categoryId) 
+                channel.setUserLimit(1)
+                if(message.content[1] !== '')
+                {
+                    channel.setUserLimit(message.content[1])
+                }
+                else
+                {
+                    channel.setUserLimit(3)
+                }
+            })
+    })
+
+    //example embed
+    // command(bot, 'embed', (message) => 
+    // {
+    //     var embed = new discord.MessageEmbed().setTitle('Example Embed')
+    //     message.channel.send(embed)
+    // })
+
+    // command(bot, 'serverinfo', (message) =>
+    // {
+    //     const { guild } = message
+    //     const { name, region, memberCount, owner, afkTimeout } = guild
+    //     const icon = guild.iconURL()
+    //     const embed = new discord.MessageEmbed()
+    //     .setTitle(`Server info for "${name}"`)
+    //     .setThumbnail(icon)
+    //     .addFields
+    //     (
+    //         {
+    //             name: 'Region',
+    //             value: region
+    //         },
+    //         {
+    //             name: 'Members',
+    //             value: memberCount
+    //         },
+    //         {
+    //             name: 'Owner',
+    //             value: owner
+    //         }
+    //     )
+    //     message.channel.send(embed)
+    // })
+
+    command(bot, 'help', (message) =>
+    {
+        message.channel.send(`
+        These are example commands:
+        **!help** - Displays help menu
+        `)
+
+        const { prefix } = config
+        bot.user.setPresence
+        ({
+            activity: 
+            {
+                name: `"${prefix}help" for help`
+            }
+        })
+    })
+
+    roleClaim(bot)
 })
 
 /*The following code block is the bulk of the commands that can actually received by the bot.
 Depending on the command passed into whatever channel in the server that is prefaced with the '!' character*/ 
-bot.on
-('message', message =>
-    {
-        //this line takes the command that is passed in and seperates the ! from the rest of the text
-        //Which is then stored in the 'args' variable
-        let args = message.content.slice(cmd.length).split(' ');
-        {
 
-            /*line 40 calls the checkPermission() method, passing in the message along with the info of the sender
-            Which will see if the user possesses the correct role to be able to call any command
-            Otherwise they are sent the 'invalidPermissions' message found in the appsettings.json file */
-            let hasRole = checkPermissionClass.checkPermission(message)
-            var invalidMessage = invalidPermissionClass.invalidPermission;
-            switch(args[0])
-            {
-                case 'apex':
-                    if(hasRole) {apexAssemblyClass.apex(message);}
-                    else {invalidMessage;}
-                break;
-                case 'apoc':
-                    if(hasRole) {shadyPlebClass.apoc(message);}
-                    else {invalidMessage;}
-                break;
-                case 'authors':
-                    if(hasRole) {message.channel.send(json.authors);}
-                    else {invalidMessage;}
-                break;
-                case 'chel':
-                    if(hasRole) {chelAssemblyClass.chel(message);}
-                    else {invalidMessage;}
-                break;
-                case 'commands':
-                    if(hasRole) {message.channel.send({embed: cmdListEmbedClass.exampleEmbed});}
-                    else {invalidMessage;}
-                break;
-                case 'discord':
-                    if(hasRole) {message.channel.send(json.discord);}
-                    else {invalidMessage;}
-                break;
-                case 'duds':
-                    if(hasRole) {shadyPlebClass.duds(message);}
-                    else {invalidMessage;}
-                break;
-                case 'ferg':
-                    if(hasRole) {shadyPlebClass.ferg(message);}
-                    else {invalidMessage;}
-                break;
-                case 'gemini':
-                    if(hasRole) {shadyPlebClass.gemini(message);}
-                    else {invalidMessage;}
-                break;
-                case 'hello':
-                    if(hasRole) {helloClass.hello(message);} 
-                    else {invalidMessage;}
-                break;
-                case 'hog':
-                    if(hasRole) {shadyPlebClass.hog(message);}
-                    else {invalidMessage;}
-                break;
-                case 'kenshin':
-                    if(hasRole) {shadyPlebClass.kenshin(message);}
-                    else {invalidMessage;}
-                break;
-                case 'kuges':
-                    if(hasRole) {shadyPlebClass.kuges(message);}
-                    else {invalidMessage;}
-                break;
-                case 'louie':
-                    if(hasRole) {shadyPlebClass.louie(message);}
-                    else{invalidMessage;}
-                break;
-                case 'player':
-                    if(hasRole) {playerInfoClass.player(message);}
-                    else {invalidMessage;}
-                break;
-                case 'raid':
-                    if(hasRole) {raidAssemblyClass.raid(message);}
-                    else {invalidMessage;}
-                break;
-                case 'sieve':
-                    if(hasRole){shadyPlebClass.sieve(message);}
-                    else {invalidMessage;}
-                break;
-                case 'SirProgrammer':
-                    if(hasRole) {sirMasterProgrammerClass.hq(message);}
-                    else {invalidMessage;}
-                break;
-                case 'twitch':
-                    if(hasRole) {message.channel.send(json.code);}
-                    else {invalidMessage;}
-                break;
-                case 'twitch3':
-                    if(hasRole) {message.channel.send(json.twitch3);}
-                    else {invalidMessage;} 
-                break;
-                case 'twitch8':
-                    if(hasRole) {message.channel.send(json.twitch8);}
-                    else {invalidMessage;} 
-                break;
-                case 'twitch16':
-                    if(hasRole) {message.channel.send(json.twitch16);}
-                    else {invalidMessage;} 
-                break;
-                case 'twitchferg':
-                    if(hasRole) {message.channel.send(json.twitchferg);} 
-                    else {invalidMessage;}
-                break;
-                case 'version':
-                    if(hasRole) {message.channel.send(package.version);}
-                    else {invalidMessage;}
-                break;
-                case 'zone':
-                    if(hasRole) {zoneAssemblyClass.warzone(message);}
-                    else {invalidMessage;}
-                break;
-            }
-            
-        }
-    }
-)
 /*
 These lines ensure that the correct user is being passed as an object from the appsettings.json file */
 console.log(`${json.Users[0].name} is user 0`)
