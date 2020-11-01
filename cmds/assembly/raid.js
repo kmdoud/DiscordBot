765dcb2934577fcc68d49071a9c904d5d7625b6f
Ken Doud <kenneth.doud@gmail.com>
2020-10-31 21:08:47 -0400

    modified raid to raidAssembly in appsettings, moved assembly commands within commando framework

---
+++ cmds/assembly/raid.js
@@ -0,0 +1,23 @@
+const commando = require('discord.js-commando')
+const json = require('@root/appsettings')
+
+module.exports = class RaidCommand extends commando.Command
+{
+    constructor(client)
+    {
+        super(client,
+            {
+                name: 'raid',
+                aliases: ['destiny', 'd2', 'd2raid'],
+                group: 'assembly',
+                memberName: 'raid',
+                description: 'Assemble the Plebs, its time to RRAAAIIIDDDDD',
+                details: 'Sends a message to all members of the server letting them know someone is playing Destiny',
+            })
+    }
+
+    async run(message, args)
+    {
+        message.channel.send(json.raidAssembly)
+    }
+}
